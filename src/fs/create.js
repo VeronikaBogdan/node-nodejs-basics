4ffbc9040025090a7673b31804f59f3c5f460d0a
VeronikaBogdan <bog-dan7777@mail.ru>
2022-06-02 02:19:17 +0300

    feat: complete create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,23 @@
+import { writeFile, access, constants } from "fs";
+import path from "path";
+
 export const create = async () => {
-    // Write your code here 
+  const __dirname = path.resolve(path.dirname(""));
+  access(path.join(__dirname, "files", "fresh.txt"), constants.F_OK, (err) => {
+    try {
+      if (err)
+        writeFile(
+          path.join(__dirname, "files", "fresh.txt"),
+          "I am fresh and young",
+          (err) => {
+            console.log("File was created");
+          }
+        );
+      if (!err) throw new Error("FS operation failed");
+    } catch (error) {
+      console.log(error);
+    }
+  });
 };
+
+await create();
